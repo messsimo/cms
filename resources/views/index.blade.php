f649d7dc83e33fe10074ba92e2c2df42059e3162
Daniel Mihai <danikmihai23@mail.ru>
2024-10-24 08:43:22 +0300

    Feature: Dashboard template(1)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,31 @@
 
 <!-- Секция -->
 @section("index")
-    <h1>Index page</h1>
-    <a href="{{ route('login_page') }}">Log In</a>
+    <!-- Общий контейнер -->
+    <div class="main-wrapper">
+    <!-- Блок навигации -->
+    <div class="cms-navigation">
+        <div class="cms-navigation--top">
+            <h2>CMS</h2>
+            <img src="{{ asset('images/anton.png') }}" alt="User image">
+            <!-- Заменить Admin в <p> на динамический параметр вошедщего юзера-->
+            <p>Admin</p> 
+        </div>
+
+        <nav>
+            <a href="{{ route('index_page') }}">Dashboard</a>
+            <hr>
+            <a href="">In proccess..</a>
+            <hr>
+            <a href="">In proccess..</a>
+            <hr>
+            <a href="">In proccess..</a>
+            <hr>
+        </nav>
+    </div>
+
+
+
+
+    </div> 
 @endsection
